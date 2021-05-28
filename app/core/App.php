642651b7e3591e0a5885311ee9de438315f81a12
Adentya Elmas Pranawa <111201912304@mhs.dinus.ac.id>
2021-05-28 13:30:51 +0700

    Update App.php

--- app/core/App.php
+++ app/core/App.php
@@ -1,6 +1,14 @@
 <?php
     class App{
         public function __construct(){
-            echo "Hai";
+            $url = $this->parseURL();
+            var_dump($url);
+        }
+
+        public function parseURL(){
+            if(isset($_GET['url'])){
+                $url = $_GET['url'];
+                return $url;
+            }
         }
     }
