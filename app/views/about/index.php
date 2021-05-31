38477b158ab5301a6f28bc782386b15c6bb778f5
Adentya Elmas Pranawa <111201912304@mhs.dinus.ac.id>
2021-05-31 09:59:09 +0700

    up

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,6 @@
-
-        <h1>About Me</h1>
-        <p>Halo, Nama saya <?= $data['nama'];?>, saya adalah seorang <?= $data['status']?></p>
+<div class="container">
+        <h1 class="mt-4">About Me</h1>
+        <img src="<?= BASEURL?>/img/poto.jpg" alt="Adentya" width="500" class="shadow">
+        <p class="mt-2">Halo, Nama saya <?= $data['nama'];?>, saya adalah seorang <?= $data['status']?></p>
+</div>
   
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,4 +1,9 @@
-
-        <h1>Selamat Datang di Website Saya</h1>
-    </body>
-
+<div class="container">
+    <div class="bg-light p-5 rounded-lg m-3">
+    <h1 class="display-4">Halo Selamat Datang</h1>
+    <p class="lead">Website Portofolio Adentya Elmas Pranawa</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+</div>
